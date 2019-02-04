a1f700c4b779a71cec30a4cbb61afeab65d8898b
Ada Šiurna <38506287+adasiurna@users.noreply.github.com>
2019-02-04 10:58:30 +0200

    added start script and port config

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const moment = require('moment');
 moment.locale('lt');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // process.env.PORT vietoje atsiras heroku portas
 
 const databaseUser = 'bta-studentas';
 const databasePassword = 'studentas123';
