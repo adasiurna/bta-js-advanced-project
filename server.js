7fcc76bd7a12684bc36f2fdeb182b154e3b41a80
Ada Šiurna <38506287+adasiurna@users.noreply.github.com>
2019-02-11 18:55:19 +0200

    fix cors

--- server.js
+++ server.js
@@ -30,9 +30,6 @@ app.get('/products/:id', function (req, res, next) {
   res.json({ msg: 'This is CORS-enabled for all origins!' })
 })
 
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 3000')
-})
 
 // database config
 mongoose.connect(
@@ -45,5 +42,4 @@ app.use('/books', booksRoutes);
 app.use('/authors', authorsRoutes);
 
 
-
-app.listen(port, () => console.log(`BTA API application listening on port ${port}!`))
+app.listen(port, () => console.log(`CORS-enabled web server listening on port ${port}!`));
